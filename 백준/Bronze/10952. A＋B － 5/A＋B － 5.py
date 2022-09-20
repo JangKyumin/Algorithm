558a9a1d423ec3d64a37bd303dd3300d79449a0c
kmjang <kyumin.jang@gmail.com>
2022-09-20 18:53:28 +0900

    [Bronze V] Title: A+B - 5, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,10 +1,4 @@
 import sys
 
-end = True
-while end:
-    a, b = map(int, sys.stdin.readline().split())
-
-    if a + b == 0:
-        quit()
-    
-    print(f"{a + b}")
+while (s := sum(map(int, sys.stdin.readline().split()))):
+    print(f"{s}")
