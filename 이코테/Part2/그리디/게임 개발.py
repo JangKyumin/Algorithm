875360befc90f0fb14b487eda301f68113b1adc4
kmjang <kyumin.jang@gmail.com>
2022-12-13 19:35:49 +0900

    게임 개발

---
+++ 이코테/Part2/그리디/게임 개발.py
@@ -0,0 +1,64 @@
+# 세로 크기 N과 가로 크기 M을 공백으로 구분하여 입력 받음
+n, m = map(int, input().split())
+
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+
+# 게임 캐릭터가 있는 칸의 좌표 (A, B)와 바라보는 방향(0: 북, 1: 동, 2: 남, 3: 서)을 입력 받는다.
+x, y, direction = map(int, input().split())
+d[x][y] = 1  # 현재 좌표 방문 처리
+
+# 맵이 육지인지 바다인지에 대한 정보가 주어진다. N개의 줄에 맵의 상태가 북쪽부터 남쪽 순서대로, 각 줄의 데이터는 서쪽부터 동쪽 순서대로 주어진다. 맵의 외각은 항상 바다이다.
+map_array = []
+for i in range(n):
+    map_array.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+# 왼쪽으로 회전
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+
+# 시물레이션 시작
+count = 1
+turn_time = 0
+while True:
+    # 왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and map_array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time += 1
+
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+
+        # 뒤로 갈 수 있다면 이동하기
+        if map_array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else:
+            break
+        turn_time = 0
+
+print(count)
