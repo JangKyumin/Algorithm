821c9248711f4b175db1eedf6078dd230e16327d
kmjang <kyumin.jang@gmail.com>
2023-01-23 19:36:39 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -2,5 +2,5 @@ def solution(my_string):
     answer = 0
     for string in my_string:
         if string.isnumeric():
-            answer += int(string)
+            answer += int(string) 
     return answer
